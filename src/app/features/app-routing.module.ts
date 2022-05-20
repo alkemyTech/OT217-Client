32003e737f7494f056a72520ea7f59c3e2a301b0
Matias Rodriguez <matiasrodriguezb@gmail.com>
2022-05-20 09:01:51 -0400

    change root path to homecomponent

--- src/app/features/app-routing.module.ts
+++ src/app/features/app-routing.module.ts
@@ -2,8 +2,13 @@ import { ActivityFormComponent } from "./pages/activities/activity-form/activity
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { RouterModule, Routes } from "@angular/router";
+import { HomeComponent } from "./pages/home/home.component";
 
 const routes: Routes = [
+  {
+    path: "",
+    component: HomeComponent
+  },
   { 
     path: "actividades", 
     component: ActivityFormComponent },
