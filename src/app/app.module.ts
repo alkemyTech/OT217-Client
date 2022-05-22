c5115bdcb0a3f259843abe2fd917e2aa3d0cb1c2
Matias Rodriguez <matiasrodriguezb@gmail.com>
2022-05-21 22:27:07 -0400

    importing organization component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,6 +19,7 @@ import { MatRadioModule } from '@angular/material/radio';
 import { MatSelectModule } from '@angular/material/select';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
+import { OrganizationComponent } from './features/pages/organization/organization.component';
 
 
 @NgModule({
@@ -48,7 +49,8 @@ import { HttpClientModule } from '@angular/common/http';
     MatCheckboxModule,
     MatIconModule,
     MatCardModule,
-    HttpClientModule
+    HttpClientModule,
+    OrganizationComponent
   ],
   exports: [
     BrowserAnimationsModule,
