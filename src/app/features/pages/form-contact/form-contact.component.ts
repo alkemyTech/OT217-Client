17bfc1191bdf100890d2168a753cd2325e322da2
Matias <matias.pizarro147@gmail.com>
2022-05-23 16:58:07 -0300

    changes modified

--- src/app/features/pages/form-contact/form-contact.component.ts
+++ src/app/features/pages/form-contact/form-contact.component.ts
@@ -1,45 +1,33 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators, FormControl} from '@angular/forms';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-form-contact',
-  templateUrl: './form-contact.component.html',
-  styleUrls: ['./form-contact.component.scss']
+  selector: "app-form-contact",
+  templateUrl: "./form-contact.component.html",
+  styleUrls: ["./form-contact.component.scss"],
 })
+export class FormContactComponent {
+  public formLogin: FormGroup;
 
-export class FormContactComponent implements OnInit {
-
-  public formLogin:FormGroup;
-
-  constructor(private formBuilder:FormBuilder) {
+  constructor(private formBuilder: FormBuilder) {
     this.formLogin = this.formBuilder.group({
-      name : ['',Validators.required],
-      telephone : ['',[Validators.required,Validators.minLength(8)]],
-      email : ['', [Validators.required,Validators.email]],
-      message: ['', Validators.required]
-    })
-   }
- 
-  ngOnInit(): void {
-    
-  }
-  
-
-  enviarDatos():any{
-    console.log("message");
+      name: ["", Validators.required],
+      telephone: ["", [Validators.required, Validators.minLength(8)]],
+      email: ["", [Validators.required, Validators.email]],
+      message: ["", Validators.required],
+    });
   }
 
-  get Name(){
-      return this.formLogin.get("name");
+  get Name() {
+    return this.formLogin.get("name");
   }
-  get Telephone(){
+  get Telephone() {
     return this.formLogin.get("telephone");
-}
-  get Email(){
-  return this.formLogin.get("email");
-}
-  get Message(){
-  return this.formLogin.get("message");
-}
-
+  }
+  get Email() {
+    return this.formLogin.get("email");
+  }
+  get Message() {
+    return this.formLogin.get("message");
+  }
 }
