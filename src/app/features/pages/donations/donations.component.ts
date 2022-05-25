36ed97ae1422186bbc573b0f2740025d6a207eae
Franco O <franco_osores@hotmail.com>
2022-05-25 18:50:28 -0300

    Feat: DOnation section created, with a button and input to choose how much to donate

--- src/app/features/pages/donations/donations.component.ts
+++ src/app/features/pages/donations/donations.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-donations',
-  templateUrl: './donations.component.html',
-  styleUrls: ['./donations.component.scss']
+  selector: "app-donations",
+  templateUrl: "./donations.component.html",
+  styleUrls: ["./donations.component.scss"],
 })
 export class DonationsComponent implements OnInit {
+  donation: number = 300;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  setDonation($event: number) {
+    this.donation = $event.valueOf();
   }
 
+  makeDonation() {
+    switch (this.donation) {
+      case 100:
+        window.open("https://mpago.la/2iv6WJp", "_blank");
+        break;
+      case 200:
+        window.open("https://mpago.la/1b9B39s", "_blank");
+        break;
+      case 300:
+        window.open("https://mpago.la/2e9nP98", "_blank");
+        break;
+      case 400:
+        window.open("https://mpago.la/2e9nP98", "_blank");
+        break;
+      case 500:
+        window.open("https://mpago.la/16kE57P", "_blank");
+        break;
+
+      default:
+        break;
+    }
+  }
+
+  constructor() {}
+
+  ngOnInit(): void {}
 }
