3d0160fcf6efe1d89296b690f47374f80157f3e3
Franco O <franco_osores@hotmail.com>
2022-05-26 23:32:35 -0300

    Save before install NgBootstrap

--- src/app/features/pages/slides/slides-list/slides-list.component.ts
+++ src/app/features/pages/slides/slides-list/slides-list.component.ts
@@ -1,19 +1,39 @@
-import { Component, OnInit } from '@angular/core';
-import { SlidesService } from 'src/app/core/services/slides.service';
-import { Slides } from 'src/app/shared/models/Slides';
+import { Component, OnInit, PipeTransform } from "@angular/core";
+import { SlidesService } from "src/app/core/services/slides.service";
+import { Slides } from "src/app/shared/models/Slides";
+import { FormControl } from "@angular/forms";
+
+import { Observable } from "rxjs";
+import { map, startWith } from "rxjs/operators";
 
 @Component({
-  selector: 'app-slides-list',
-  templateUrl: './slides-list.component.html',
-  styleUrls: ['./slides-list.component.scss']
+  selector: "app-slides-list",
+  templateUrl: "./slides-list.component.html",
+  styleUrls: ["./slides-list.component.scss"],
 })
 export class SlidesListComponent implements OnInit {
-  slidesDataTable:Slides[] = [];
-
-  constructor(private slidesService:SlidesService) { }
+  slidesDataTable: Slides[] = [];
 
   ngOnInit(): void {
-    this.slidesService.getSlides().subscribe(response =>{
+    this.slidesService.getSlides().subscribe((response) => {
       this.slidesDataTable = response.data;
-    })}
+    });
+  }
+
+  search(text: string): Slides[] {
+    return this.slidesDataTable.filter((slide) => {
+      const term = text.toLowerCase();
+      return slide.name.toLowerCase().includes(term);
+    });
+  }
+
+  slides$: Observable<Slides[]>;
+  filter = new FormControl("");
+
+  constructor(private slidesService: SlidesService) {
+    this.slides$ = this.filter.valueChanges.pipe(
+      startWith(""),
+      map((text) => this.search(text))
+    );
+  }
 }
