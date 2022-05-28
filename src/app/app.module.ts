513fdd23c1ffd724475415bb02a6449bf086b72b
ms-cris <personalmc@vivaldi.net>
2022-05-27 20:43:10 -0400

    new declaratons for new components footer navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { FeaturesModule } from './features/features.module';
 import { CoreModule } from './core/core.module';
-import { NgModule} from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
+import { FooterComponent } from './features/pages/home/footer/footer.component';
+import { HomeComponent } from './features/pages/home/home.component';
+import { NavbarComponent } from './features/pages/home/navbar/navbar.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    FooterComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
