7f6789aeaa806f5f0aaa3cbef80c479cb9a28be4
ms-cris <personalmc@vivaldi.net>
2022-05-29 00:43:05 -0400

    configuring validators and html

--- src/app/features/pages/organization/organization-edit/organization-edit.component.ts
+++ src/app/features/pages/organization/organization-edit/organization-edit.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
+import * as ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+
+
+
 
 @Component({
   selector: 'app-organization-edit',
@@ -7,9 +13,64 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrganizationEditComponent implements OnInit {
 
-  constructor() { }
+  public Editor = ClassicEditor;
+  myForm: FormGroup;
+  urlImg: string = "";
+  public myreg = ('^(https?:\/\/)*[a-z0-9-]+(\.[a-z0-9-]+)+(\/[a-z0-9-]+)*\/?$');
+
+
+  constructor(private fb: FormBuilder, public dialog: MatDialog) {
+    this.myForm = this.fb.group({
+      name: ['', [Validators.required]],
+      logo: ['', [Validators.required]],
+      description: ['', [Validators.required]],
+      description2: ['', [Validators.required]],
+      urlfacebook: ['', [Validators.pattern(this.myreg), Validators.required, Validators.minLength(23)]],
+      urltwitter: ['', [Validators.pattern(this.myreg), Validators.required, Validators.minLength(22)]],
+      urlinstagram: ['', [Validators.pattern(this.myreg), Validators.required, Validators.minLength(24)]],
+      urllinkedin: ['', [Validators.pattern(this.myreg), Validators.required, Validators.minLength(23)]]
+    });
+  }
+
+
 
   ngOnInit() {
   }
 
+  ckeditorContent: any;
+
+
+
+  addDescription() {
+
+  };
+
+
+
+
+
+
+
+  onFileChange(event: any) {
+    const reader = new FileReader();
+
+
+
+
+
+    if (event.target.files && event.target.files.length) {
+      const [file] = event.target.files;
+
+      reader.readAsDataURL(file);
+
+      reader.onload = () => {
+        this.urlImg = reader.result as string;
+      }
+    }
+
+  }
+
 }
+
+
+
