61a89807d18dedb5ee0029b795578ebae285740b
Matias <matias.pizarro147@gmail.com>
2022-05-30 00:16:33 -0300

    adecuando codigo

--- src/app/core/services/publicapi-service.service.ts
+++ src/app/core/services/publicapi-service.service.ts
@@ -1,22 +1,20 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class PublicapiServiceService {
+  baseUrl: string = "https://ongapi.alkemy.org/api/";
 
-  baseUrl:string = "https://ongapi.alkemy.org/api/"
-
-  rutaendpoint = (id:any) =>{
+  rutaendpoint = (id: any) => {
     return this.baseUrl + id;
-  }
+  };
 
-  constructor(private http:HttpClient) { }
+  constructor(private http: HttpClient) {}
 
-
-  metodoGet(id:any, users:any):Observable<any>{
-    return this.http.get(this.rutaendpoint(id || null),users)
+  metodoGet(id: any): Observable<any> {
+    return this.http.get(this.rutaendpoint(id || null));
   }
 }
