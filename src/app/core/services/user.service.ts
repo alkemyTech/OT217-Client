a53e542c6a2a41e2504580a339db52ae2abe0392
Matias <matias.pizarro147@gmail.com>
2022-05-30 00:05:14 -0300

    dar formato con prettier

--- src/app/core/services/user.service.ts
+++ src/app/core/services/user.service.ts
@@ -1,34 +1,30 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { Users } from 'src/app/shared/model/Users';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { Users } from "src/app/shared/model/Users";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class UserService {
+  constructor(private http: HttpClient) {}
 
-  constructor(private http:HttpClient) { }
+  url: string = "https://ongapi.alkemy.org/api/users";
 
-   url:string = "https://ongapi.alkemy.org/api/users"
-  
-   
-   postUser(user:any):Observable<any>{
+  postUser(user: any): Observable<any> {
+    return this.http.post(this.url, user);
+  }
 
-    return this.http.post(this.url, user)
-   }
+  putUser(user: any, id: string) {
+    return this.http.put(this.url + `/${id}`, user);
+  }
 
-   putUser(user:any,id:string){
-     return this.http.put(this.url + `/${id}`, user)
-   }
+  getUser(): Observable<any> {
+    return this.http.get(this.url);
+  }
 
-   getUser():Observable<any>{
-     return this.http.get(this.url) 
-   }
-
-   deleteUser(user:Users):Observable<Users>{
-     const url = `${this.url}/${user.id}`
-     return this.http.delete<Users>(url)
-   }
-
+  deleteUser(user: Users): Observable<Users> {
+    const url = `${this.url}/${user.id}`;
+    return this.http.delete<Users>(url);
+  }
 }
