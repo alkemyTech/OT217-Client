6573eaa86b5a83d7ba7f6d2469e53002bbbd29af
ms-cris <personalmc@vivaldi.net>
2022-05-30 20:09:25 -0400

    binding data to inputs

--- src/app/features/pages/organization/organization-edit/organization-edit.component.ts
+++ src/app/features/pages/organization/organization-edit/organization-edit.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
 import * as ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import { OrganizationService } from "src/app/core/services/organization.service";
 
 
 
@@ -17,24 +18,45 @@ export class OrganizationEditComponent implements OnInit {
   myForm: FormGroup;
   urlImg: string = "";
   public myreg = ('^(https?:\/\/)*[a-z0-9-]+(\.[a-z0-9-]+)+(\/[a-z0-9-]+)*\/?$');
+  public listaDatos: Array<any> = [];
+  nameInput!: any;
+  descriptionInput: any;
+  description2Input: any;
+  urlfacebookInput: any;
+  urltwitterInput: any;
+  urlinstagramInput: any;
+  urllinkedinInput: any;
 
 
-  constructor(private fb: FormBuilder, public dialog: MatDialog) {
+
+  constructor(private fb: FormBuilder, public dialog: MatDialog, private organizationService: OrganizationService) {
+
     this.myForm = this.fb.group({
       name: ['', [Validators.required]],
       logo: ['', [Validators.required]],
       description: ['', [Validators.required]],
       description2: ['', [Validators.required]],
-      urlfacebook: ['', [Validators.pattern(this.myreg), Validators.required, Validators.minLength(23)]],
-      urltwitter: ['', [Validators.pattern(this.myreg), Validators.required, Validators.minLength(22)]],
-      urlinstagram: ['', [Validators.pattern(this.myreg), Validators.required, Validators.minLength(24)]],
-      urllinkedin: ['', [Validators.pattern(this.myreg), Validators.required, Validators.minLength(23)]]
+      urlfacebook: ['', [Validators.pattern(this.myreg), Validators.required]],
+      urltwitter: ['', [Validators.pattern(this.myreg), Validators.required]],
+      urlinstagram: ['', [Validators.pattern(this.myreg), Validators.required, Validators.minLength(25)]],
+      urllinkedin: ['', [Validators.pattern(this.myreg), Validators.required, Validators.minLength(24)]]
     });
   }
 
 
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.organizationService.topOrganization().subscribe((result: any) => {
+      this.listaDatos = result.data;
+      console.log(result.data);
+      this.nameInput = result.data.name;
+      this.descriptionInput = result.data.long_description;
+      this.description2Input = result.data.short_description;
+      this.urlfacebookInput = result.data.facebook_url;
+      this.urltwitterInput = result.data.twitter_url;
+      this.urlinstagramInput = result.data.instagram_url;
+      this.urllinkedinInput = result.data.linkedin_url;
+    });
   }
 
   ckeditorContent: any;
