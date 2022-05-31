b29b0341354e93273ffc1a40b68c44eafd7b61e9
ralphHuerta <huerta.1996@hotmail.com>
2022-05-31 15:59:11 -0500

    prettier

--- src/app/features/pages/activities/activities-list/activities-list.component.ts
+++ src/app/features/pages/activities/activities-list/activities-list.component.ts
@@ -1,36 +1,34 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { ActivitiesServices } from 'src/app/core/services/activities.service';
-import { Activity } from 'src/app/shared/models/Activity';
+import { Component, OnInit } from "@angular/core";
+import { ActivitiesServices } from "src/app/core/services/activities.service";
+import { Activity } from "src/app/shared/models/Activity";
 @Component({
-  selector: 'app-activities-list',
-  templateUrl: './activities-list.component.html',
-  styleUrls: ['./activities-list.component.scss'],
-  providers:[ ActivitiesServices]
+  selector: "app-activities-list",
+  templateUrl: "./activities-list.component.html",
+  styleUrls: ["./activities-list.component.scss"],
+  providers: [ActivitiesServices],
 })
 export class ActivitiesListComponent implements OnInit {
-  public activities: Activity[]=[]
+  public activities: Activity[] = [];
 
-  constructor(private _ActivitiesServices:ActivitiesServices) { }
+  constructor(private _ActivitiesServices: ActivitiesServices) {}
 
   ngOnInit(): void {
-    this.getActivities()
+    this.getActivities();
   }
 
-  getActivities(){
+  getActivities() {
     this._ActivitiesServices.getActiviti().subscribe(
-      response =>{
-        this.activities=response.data
-      },error =>{
-        console.log(<any> error)
-      }
-    )
+      (response) => {
+        this.activities = response.data;
+      },
+      (error) => {}
+    );
   }
 
-  deleteActivity= (event: any):void =>{
+  deleteActivity = (event: any): void => {
     let id = String(event);
-    this._ActivitiesServices.deleteActivities(id).subscribe(()=>{
+    this._ActivitiesServices.deleteActivities(id).subscribe(() => {
       this.getActivities();
     });
-  }
-  
+  };
 }
