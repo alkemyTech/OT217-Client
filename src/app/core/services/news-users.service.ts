ee8eef78da807cf7e8cbcea2479c52f5a4de0d39
Matias <matias.pizarro147@gmail.com>
2022-05-31 19:34:27 -0300

    cambios realizados a extender a publicservice

--- src/app/core/services/news-users.service.ts
+++ src/app/core/services/news-users.service.ts
@@ -8,23 +8,25 @@ import { PublicapiServiceService } from './publicapi-service.service';
 })
 export class NewsUsersService extends PublicapiServiceService{
 
-  constructor(http:HttpClient) {
+  constructor(http:HttpClient, ) {
     super(http)
    }
 
    getUsers<Users>():Observable<Users>{
-     return this.get<Users>();
-   }
-   postUsers<Users>(users:Users): Observable<Users>{
-     return this.post<Users>(users);
-   }
-   putUsers<Users>(users:any, id:string):Observable<Users>{
+    return this.get<Users>('users');
+  }
+  postUsers<Users>(users:Users):Observable<Users>{
+    return this.post<Users>(users);
+  }
+
+  putUsers<Users>(users:any, id:string):Observable<Users>{
     return this.put<Users>(id,users);
   }
-  getSlidesById<Users>(id:string):Observable<Users>{
-    return this.getById<Users>(id);
+  getUsersById<Users>(id:string):Observable<Users>{
+    return this.getById<Users>('users/'+id);
   }
   deleteUsers<Users>(id:string):Observable<Users>{
-    return this.delete<Users>(id);
+    return this.delete<Users>("users/" +id);
   }
+
 }
--- src/app/core/services/publicapi-service.service.ts
+++ src/app/core/services/publicapi-service.service.ts
@@ -7,29 +7,30 @@ import { Observable } from 'rxjs';
 })
 export class PublicapiServiceService {
 
-  protected baseUrl: string = "https://ongapi.alkemy.org/api/users";
+  protected baseUrl: string = "https://ongapi.alkemy.org/api/";
 
 
  
-  constructor(private http:HttpClient) { }
+  constructor(public http:HttpClient) { }
 
 
-  public get<Users>(): Observable<Users> {
-    return this.http.get<Users>(this.baseUrl);
-  }
-  public put<Users>(body: any, id: string): Observable<Users> {
-    return this.http.put<Users>(this.baseUrl + id, body);
+  public post<T>(body: any): Observable<T> {
+    return this.http.post<T>(this.baseUrl, body);
   }
 
-  public getById<Users>(id: string): Observable<Users> {
-    return this.http.get<Users>(this.baseUrl + id);
+  public get<T>(endpoint:string): Observable<T> {
+    return this.http.get<T>(this.baseUrl+endpoint);
   }
 
-  public delete<Users>(id: string): Observable<Users> {
-    return this.http.delete<Users>(this.baseUrl + id);
+  public put<T>(body: any, id: string): Observable<T> {
+    return this.http.put<T>(this.baseUrl + id, body);
   }
 
-  public post<Users>(body:any):Observable<Users> {
-    return this.http.post<Users>(this.baseUrl, body);
+  public getById<T>(id: string): Observable<T> {
+    return this.http.get<T>(this.baseUrl+id);
+  }
+
+  public delete<T>(id: string): Observable<T> {
+    return this.http.delete<T>(this.baseUrl + id);
   }
 }
