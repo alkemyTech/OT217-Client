25efc4e063c7c3941c5faa4118312e39c13e362b
Franco O <franco_osores@hotmail.com>
2022-06-01 21:32:06 -0300

    Feat: News Endpoint added to environments, environment file added to gitIgnore

--- src/app/core/services/news.service.ts
+++ src/app/core/services/news.service.ts
@@ -2,34 +2,38 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { PublicApiService } from './public-api.service';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NewsService extends PublicApiService {
 
+  endpoint:string = environment.news;
+
   public constructor(http:HttpClient)  {
     super(http);
   }
 
   postNews<News>(news:any):Observable<any>{
-    return this.post<News>(news);
+    console.log(this.endpoint)
+    return this.post<News>(this.endpoint,news);
   }
 
   getNews<News>():Observable<any>{
-    return this.get<News>('news');
+    return this.get<News>(this.endpoint);
   }
 
   putNews<News>(news:any, id:string):Observable<News>{
-    return this.put<News>(id,news);
+    return this.put<News>(this.endpoint + "/" + id, news);
   }
 
   getNewsById<News>(id:string):Observable<News>{
-    return this.getById<News>('news/'+id);
+    return this.getById<News>(this.endpoint+ "/" + id);
   }
 
   deleteNews<News>(id:string):Observable<News>{
-    return this.delete<News>("news/" +id);
+    return this.delete<News>(this.endpoint+ "/" + id);
   }
   
 }
