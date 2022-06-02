5c05639da572962fa5a58fb320a779f69a8d9c25
Fran <vidotto982@gmail.com>
2022-06-02 11:23:39 -0300

    add slidesUrl in Environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  slidesUrl : `http://localhost:4200/backoffice/slides/getSlides`,
+  slidesEditUrl: `http://localhost:4200/backoffice/slides/putSlides`,
+  slidesDeleteUrl: `http://localhost:4200/backoffice/slides/deleteSlides`,
+
 };
