bdafd31ea8b5f07dbd1e00de9e8a9d184ba388c3
ms-cris <personalmc@vivaldi.net>
2022-06-03 12:46:14 -0400

    return to old table

--- src/app/features/pages/organization/organization-list.component.ts
+++ src/app/features/pages/organization/organization-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, OnInit } from "@angular/core";
 import { OrganizationListService } from "src/app/core/services/organization-list.service";
-import { listData } from "src/app/shared/models/listData";
+
 
 
 @Component({
@@ -15,43 +15,25 @@ export class OrganizationListComponent implements OnInit {
 
 
 
-  ngOnInit(): void { }
+  ngOnInit(): void {
+    this.organizationListService.topOrganization().subscribe((response) => {
+      this.listaDatos = response.data;
+      console.log(response.data);
+    })
+  }
 
-  @Input() title: string = "";
-  // Array to populate Table
-  @Input() dataArray: any;
-  // Definition of column names
-  @Input() firstCol: string = " ";
-  @Input() secondCol: string = " ";
-  @Input() thirdCol: string = " ";
-  @Input() fourthCol: string = " ";
-  // Action Button definition
-  @Input() editUrl: any;
-  @Input() deleteElement: any = (args: any) => { };
 
-  //Data to be displayed in columns
-  @Input() orderTrue: any;
-  @Input() imageTrue: any;
-  @Input() descriptionTrue: any;
-  @Input() createdTrue: any;
-  @Input() emailTrue: any;
 
-  displayedColumns: string[] = ["firstCol", "secondCol", "thirdCol", "order", "image", "description", "buttons"];
 
 
 
 
 
   constructor(private organizationListService: OrganizationListService) {
-    this.populateArray();
+
   }
 
-  populateArray() {
-    this.organizationListService.topOrganization().subscribe((response) => {
-      this.listaDatos = response.data;
-      console.log(response.data);
-    });
-  }
+
 
 
 };
