c6cc5ce8bceaf011c1349669832304e391de7015
ms-cris <personalmc@vivaldi.net>
2022-06-06 10:30:36 -0400

    changes requested on pull request

--- src/app/features/pages/slides/slides-form/slides-form.component.ts
+++ src/app/features/pages/slides/slides-form/slides-form.component.ts
@@ -100,14 +100,14 @@ export class SlidesFormComponent implements OnInit {
       image: this.cardImageBase64,
     };
 
-    /* if (this.slidesId) {
+    if (this.slidesId) {
       this.slidesService
         .putSlides(slidesCommit, this.slidesId)
-        .subscribe((response) => {});
+        .subscribe((response) => { });
       this.slides.reset();
     } else if (!this.slidesId) {
-      this.slidesService.postSlides(slidesCommit).subscribe((response) => {});
+      this.slidesService.postSlides(slidesCommit).subscribe((response) => { });
       this.slides.reset();
-    } */
+    }
   }
 }
