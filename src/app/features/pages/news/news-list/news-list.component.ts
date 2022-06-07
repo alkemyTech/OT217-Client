bba09dd6e804eae42b16ff76eeb50be5da4170b4
Franco O <franco_osores@hotmail.com>
2022-06-07 00:03:35 -0300

    rendering of component working, ready for effect config

--- src/app/features/pages/news/news-list/news-list.component.ts
+++ src/app/features/pages/news/news-list/news-list.component.ts
@@ -1,18 +1,37 @@
-import { Component, OnInit } from '@angular/core';
-import { Store } from '@ngrx/store';
-import { loadNews } from 'src/app/shared/state/news/news.actions';
+import { Component, OnInit } from "@angular/core";
+import { Store } from "@ngrx/store";
+import { Observable } from "rxjs";
+import { NewsService } from "src/app/core/services/news.service";
+import { AppState } from "src/app/shared/state/app.state";
+import { loadedNews, loadNews } from "src/app/shared/state/news/news.actions";
+import {
+  selectNewsLoading,
+  selectNewsList,
+} from "src/app/shared/state/news/news.selectors";
 
 @Component({
-  selector: 'app-news-list',
-  templateUrl: './news-list.component.html',
-  styleUrls: ['./news-list.component.scss']
+  selector: "app-news-list",
+  templateUrl: "./news-list.component.html",
+  styleUrls: ["./news-list.component.scss"],
 })
 export class NewsListComponent implements OnInit {
+  newsList$: Observable<any> = new Observable();
+  loading$: Observable<boolean> = new Observable();
 
-  constructor(private store:Store<any>) { }
+  constructor(
+    private store: Store<AppState>,
+    private newsService: NewsService
+  ) {}
 
   ngOnInit(): void {
-    this.store.dispatch(loadNews())
-  }
+    this.loading$ = this.store.select(selectNewsLoading);
 
+    this.store.dispatch(loadNews());
+
+    this.newsService.getNews().subscribe((response) => {
+      this.store.dispatch(loadedNews({ news: response.data }));
+    });
+
+    this.newsList$ = this.store.select(selectNewsList);
+  }
 }
--- src/app/shared/state/news/news.reducers.ts
+++ src/app/shared/state/news/news.reducers.ts
@@ -1,12 +1,15 @@
 import { createReducer, on } from "@ngrx/store";
 import { newsState } from "../../models/news.state";
-import { loadNews } from "./news.actions";
+import { loadedNews, loadNews } from "./news.actions";
 
-export const initialState:newsState={ loading: false, news: []}
+export const initialState: newsState = { loading: false, news: [] };
 
 export const newsReducer = createReducer(
-    initialState,
-    on(loadNews, (state) =>{
-        return {...state, loading: true}
-    })
-)
+  initialState,
+  on(loadNews, (state) => {
+    return { ...state, loading: true };
+  }),
+  on(loadedNews, (state, { news }) => {
+    return { ...state, loading: false, news: news };
+  })
+);
