ef2405bcdeec427521db29fc8b33288a4d7a5369
ms-cris <personalmc@vivaldi.net>
2022-06-12 17:53:25 -0400

    new navbar message when login and scss adjust

--- src/app/features/pages/public-pages/home/navbar/navbar.component.ts
+++ src/app/features/pages/public-pages/home/navbar/navbar.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/core/services/user.service';
+
+
+
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +11,60 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  activo: any;
+
+  constructor(private userService: UserService) { }
+
+  public isLogged: boolean = false;
 
   ngOnInit() {
+    this.onCheckUser();
   }
 
+  onCheckUser() {
+    if (this.userService.getUser() == null) {
+      this.isLogged = false;
+    } else {
+      this.isLogged = true;
+    }
+  }
+
+
+  rutas: any[] = [
+    {
+      path: "",
+      titulo: "Inicio"
+    },
+    {
+      path: "/nosotros",
+      titulo: "Nosotros"
+    },
+    {
+      path: "/contacto",
+      titulo: "Contacto"
+    },
+    {
+      path: "/campana_materiales_esc",
+      titulo: "Campaña materiales esc"
+    },
+    {
+      path: "/campana_juguetes",
+      titulo: "Campaña juguetes"
+    },
+
+  ]
+
+  rutasboton: any[] = [
+    {
+      path: "/login",
+      titulo: "Login"
+
+    },
+    {
+      path: "/register",
+      titulo: "Registrate"
+    }
+  ]
+
 }
+
