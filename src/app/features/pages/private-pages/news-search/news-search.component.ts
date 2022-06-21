0f8bcfe5d8eab593dafecb48e423b7d8026a90f0
ms-cris <personalmc@vivaldi.net>
2022-06-20 20:59:31 -0400

    adjusting design and colors and clean code

--- src/app/features/pages/private-pages/news-search/news-search.component.ts
+++ src/app/features/pages/private-pages/news-search/news-search.component.ts
@@ -3,10 +3,6 @@ import { fromEvent } from 'rxjs';
 import { debounceTime, map, distinctUntilChanged, filter } from 'rxjs/operators';
 import { NewsSearchService } from 'src/app/core/services/news-search.service';
 
-
-
-
-
 @Component({
   selector: 'app-news-search',
   templateUrl: './news-search.component.html',
@@ -18,12 +14,9 @@ export class NewsSearchComponent {
   txtQuery: ElementRef;
   search: any;
   showNews: any;
-
-
+  messageError: any;
 
   constructor(private newsSearchService: NewsSearchService) { }
-
-
 
   ngOnInit() {
   }
@@ -38,15 +31,12 @@ export class NewsSearchComponent {
       distinctUntilChanged(),
     ).subscribe(() => {
       this.newsSearchService.getQuery(this.search).subscribe((result: any) => {
-        console.log(result);
         this.showNews = result.data;
       })
     }, (err: any) => {
-      console.log('error', err);
+      this.messageError = 'error', err;
     });
-
   }
-
 }
 
 
