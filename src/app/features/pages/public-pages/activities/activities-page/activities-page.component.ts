c255607232c3165160361a9dd5ffc3bd213c4984
ralphHuerta <huerta.1996@hotmail.com>
2022-06-20 22:27:13 -0500

    search activities public

--- src/app/features/pages/public-pages/activities/activities-page/activities-page.component.ts
+++ src/app/features/pages/public-pages/activities/activities-page/activities-page.component.ts
@@ -2,6 +2,14 @@ import { Component, OnInit } from "@angular/core";
 import { ActivityService } from "src/app/core/services/activity.service";
 import { Activity } from "src/app/shared/models/Activity";
 import { PageEvent } from "@angular/material/paginator";
+import {
+  debounceTime,
+  distinctUntilChanged,
+  switchMap,
+  tap,
+} from "rxjs/operators";
+import { Subject } from "rxjs";
+import { ActivitiesServices } from "src/app/core/services/activities.service";
 
 @Component({
   selector: "app-activities-page",
@@ -11,7 +19,12 @@ import { PageEvent } from "@angular/material/paginator";
 export class ActivitiesPageComponent implements OnInit {
   activities: Activity[] = [];
 
-  constructor(private activitiesService: ActivityService) {}
+  private searchTerms = new Subject<string>();
+
+  constructor(
+    private activitiesService: ActivityService,
+    private activitieService: ActivitiesServices
+  ) {}
 
   page_size: number = 3;
   page_number: number = 1;
@@ -22,8 +35,30 @@ export class ActivitiesPageComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.loadActivities();
+    this.searchTerms
+      .pipe(
+        debounceTime(300),
+        distinctUntilChanged(),
+        switchMap((term: string) =>
+          this.activitieService.getActivitiesSearch(term)
+        ),
+        tap((activiti: any) => {
+          this.activities = activiti.data;
+        })
+      )
+      .subscribe();
+  }
+  loadActivities() {
     this.activitiesService.getActivities().subscribe((response) => {
       this.activities = response.data;
     });
+  }
+  shearsActie(search: any) {
+    if (search.length >= 3) {
+      this.searchTerms.next(search);
+    } else {
+      this.loadActivities();
+    }
   }
 }
