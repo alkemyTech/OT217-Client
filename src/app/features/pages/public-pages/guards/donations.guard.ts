d525ac218c2953794075e188996a9d11055c1b5a
Franco Osores <franco_osores@hotmail.com>
2022-06-21 22:21:31 -0300

    Changes requested: Alert erased

--- src/app/features/pages/public-pages/guards/donations.guard.ts
+++ src/app/features/pages/public-pages/guards/donations.guard.ts
@@ -7,13 +7,12 @@ import {
   UrlTree,
 } from "@angular/router";
 import { Observable } from "rxjs";
-import { AlertsService } from "src/app/core/services/alerts.service";
 
 @Injectable({
   providedIn: "root",
 })
 export class DonationsGuard implements CanActivate {
-  constructor(private alert: AlertsService, private route: Router) {}
+  constructor(private route: Router) {}
 
   canActivate(
     route: ActivatedRouteSnapshot,
@@ -26,7 +25,6 @@ export class DonationsGuard implements CanActivate {
     const role = localStorage.getItem("userId");
 
     if (role === 'admin') {
-      this.alert.adminDonation()
       this.route.navigate(['backoffice'])
       return false;
     } else {
