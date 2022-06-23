4c1dd8c4e17407c478132a9a8bc23a68227a1114
Matias <matias.pizarro147@gmail.com>
2022-06-22 22:52:23 -0300

    agregando el slider de inicio

--- src/app/features/pages/public-pages/slides/slide-inicio/slide-inicio.component.ts
+++ src/app/features/pages/public-pages/slides/slide-inicio/slide-inicio.component.ts
@@ -1,15 +1,24 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from "@angular/core";
+import { SlidesService } from "src/app/core/services/slides.service";
+import { Slides } from "src/app/shared/models/Slides";
 
 @Component({
-  selector: 'app-slide-inicio',
-  templateUrl: './slide-inicio.component.html',
-  styleUrls: ['./slide-inicio.component.scss']
+  selector: "app-slide-inicio",
+  templateUrl: "./slide-inicio.component.html",
+  styleUrls: ["./slide-inicio.component.scss"],
 })
 export class SlideInicioComponent implements OnInit {
-  @Input() card:any;
-  constructor() { }
+  @Input() card: any;
+  slides: Slides[] = [];
+  constructor(private slideService: SlidesService) {}
 
   ngOnInit(): void {
+    this.getSlides();
   }
 
+  getSlides() {
+    this.slideService.getSlides().subscribe((response) => {
+      this.slides = response.data;
+    });
+  }
 }
