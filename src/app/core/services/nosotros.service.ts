a47cd0d6c6a9c045c8a995f750b24646dd0eda40
rcf7 <rodrigocampanaferrer@gmail.com>
2022-06-25 13:41:15 -0300

    Get Request

--- src/app/core/services/nosotros.service.ts
+++ src/app/core/services/nosotros.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Organization } from 'src/app/features/models';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NosotrosService {
+  
+  private urlApi = "https://ongapi.alkemy.org/api/organization";
+  
+  constructor(private http: HttpClient) { 
 
-  constructor() { }
+  }
+    
+  
+
+  getOrganization(): Observable<Organization[]> {
+    return this.http.get<Organization[]>(this.urlApi)
+  }
+
+ 
+
+
+
+
 }
+
+
+
+
